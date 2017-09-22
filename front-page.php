b662b34628be301afb66612f4a8744e73dba6b43
grich89 <richg@merrimack.edu>
2017-09-22 17:01:54 -0400

    first commit

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php
+get_header(); ?>
+
+<div class="bighead">
+    <h1>Greg</h1>
+    <h2>Rich</h2>
+</div>
+
+<p>I have done so many amazing things. Please, let me tell you about them. Sit down, grab a drink, sip that cola, nourish it, shit on it, put it in a blender, vomit on it.</p>
+<h2>You are now ready to browse my website.</h2>
+
+<?php
+get_footer(); ?>
---
+++ header.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package comic
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'comic' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><span class="first">Greg</span><span class="last">Rich</span></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><span class="first">Greg</span><span class="last">Rich</span></a></p>
+			<?php
+			endif;
+
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ) : ?>
+				<p class="site-description"><span class="designer"><span class="a">D</span><span class="b">e</span><span class="c">s</span><span class="d">i</span><span class="e">g</span><span class="f">n</span><span class="g">e</span><span class="h">r</span></span><span class="developer">Developer</span></p>
+			<?php
+			endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span class="nav-icon"></span></button>
+			<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
